96a6795cd1e16fb3630ba0ea7bfd74e8dca6a741
m0ar <edvard@hubinette.me>
2025-02-14 10:52:29 +0100

    Use shared codex context in env

--- createEnv.sh
+++ createEnv.sh
@@ -2,8 +2,6 @@
 
 read -p "Paste your OrbisDB environment: " ENVIRONMENT
 
-read -p "Paste your OrbisDB context: " CONTEXT
-
 read -sp "Paste your private key from wallet: " PRIVATE_KEY
 echo # Add a newline after silent input
 
@@ -12,7 +10,6 @@ cp .env.example .env
 
 # Replace environment and private key
 sed -i "s|ORBIS_ENVIRONMENT=.*|ORBIS_ENVIRONMENT=$ENVIRONMENT|" .env
-sed -i "s|ORBIS_CONTEXT=.*|ORBIS_CONTEXT=$CONTEXT|" .env
 sed -i "s|PRIVATE_KEY=.*|PRIVATE_KEY=$PRIVATE_KEY|" .env
 
 echo
